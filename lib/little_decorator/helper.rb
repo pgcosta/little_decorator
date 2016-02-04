bce426d42aad050202f223ecae1a9831ec772851
Pedro Costa <pedroxenix@gmail.com>
2016-02-04 12:00:52 +0100

    Search for superclasses of the class to be decorated. Ie: maybe HondaDecorator does not exist but CarDecoratoe exists, Car < Honda

--- lib/little_decorator/helper.rb
+++ lib/little_decorator/helper.rb
@@ -1,17 +1,32 @@
 class LittleDecorator
   module Helper
-
+    
     def decorate(item_or_collection)
       if item_or_collection.respond_to?(:map)
         item_or_collection.map{ |item| decorate(item) }
       else
         item = item_or_collection
         return item if LittleDecorator === item
-        decorator = "#{item.class}Decorator".constantize
-        decorator.new(item, self)
+        klass = item.class.to_s
+
+        # KlassDecorator exists? if not does SuperKlassDecorator exist? and so on until the superclass is BasicObject
+        while klass != "BasicObject"
+          decorator_klass = "#{klass}Decorator"
+          if class_exists?(decorator_klass)
+            decorator = "#{decorator_klass}".constantize
+            return decorator.new(item, self)
+          else
+            klass = "#{klass}".constantize.superclass.to_s
+          end
+        end
       end
     end
+
     alias_method :d, :decorate
+
+    def class_exists?(klass)
+      Object.const_get(:"#{decorator_klass}").is_a?(Class) rescue false
+    end
 
   end
 end
